851ffdb32899e14d0ff6588ed0fa80bf0894759d
Prakhar Dogra <pdogra@cafex.com>
2018-02-25 13:29:39 -0500

    Commit

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -151,7 +151,8 @@ class LearningAgent(Agent):
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
         
-        self.Q[state][action] = self.alpha*reward + (1 - self.alpha)*self.Q[state][action]
+        if self.learning:
+            self.Q[state][action] = self.alpha*reward + (1 - self.alpha)*self.Q[state][action]
 
         return
 
